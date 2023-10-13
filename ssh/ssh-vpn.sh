1136f9e156ce55807d70303766be730b09ee9fc0
figogove <102362899+figogove@users.noreply.github.com>
2023-10-13 12:45:40 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -26,19 +26,19 @@ fi
 clear
 # ==================================================
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/lizsvr/cfnfree/main/ssh"
+akbarvpn="raw.githubusercontent.com/figogove/cfnfree/main/ssh"
 
 # Link Hosting Kalian Untuk Xray
-akbarvpnn="raw.githubusercontent.com/lizsvr/cfnfree/main/xray"
+akbarvpnn="raw.githubusercontent.com/figogove/cfnfree/main/xray"
 
 # Link Hosting Kalian Untuk Trojan Go
-akbarvpnnn="raw.githubusercontent.com/lizsvr/cfnfree/main/trojango"
+akbarvpnnn="raw.githubusercontent.com/figogove/cfnfree/main/trojango"
 
 # Link Hosting Kalian Untuk Stunnel5
-akbarvpnnnn="raw.githubusercontent.com/lizsvr/cfnfree/main/stunnel5"
+akbarvpnnnn="raw.githubusercontent.com/figogove/cfnfree/main/stunnel5"
 
 #Link Hosting Kalian Untuk Websocket
-akbarvpnnnnn="raw.githubusercontent.com/lizsvr/cfnfree/main/websocket"
+akbarvpnnnnn="raw.githubusercontent.com/figogove/cfnfree/main/websocket"
 
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
